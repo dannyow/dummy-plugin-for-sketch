59a0ad670404c1924d32e6bc4114e33a240f46f7
Daniel <danny@buffered.me>
2019-07-16 12:09:54 +0200

    Initial Commit

---
+++ DummyPlugin/DummyPlugin.h
@@ -0,0 +1,19 @@
+//
+//  DummyPlugin.h
+//  DummyPlugin
+//
+//  Created by Daniel on 16/07/2019.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyPlugin.
+FOUNDATION_EXPORT double DummyPluginVersionNumber;
+
+//! Project version string for DummyPlugin.
+FOUNDATION_EXPORT const unsigned char DummyPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyPlugin/PublicHeader.h>
+
+
